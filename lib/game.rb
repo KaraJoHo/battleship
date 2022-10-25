153ab25491e8dcbfb3756d238c1d30835803009d
KaraJH <karas.jones@yahoo.com>
2022-10-25 16:11:50 -0600

    feat: Completes the game's turn method and ends the game successfully

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,11 @@
 class Game
-  attr_reader :computers_board, :users_board
+  attr_reader :computers_board, :users_board, :computer_ships, :user_ships, :user_cruiser, :user_submarine, :computer_cruiser, :computer_submarine
 
   def computer_board
     @computers_board = Board.new
-    computer_cruiser = Ship.new("Cruiser", 3)
-    computer_submarine = Ship.new("Submarine", 2)
-    computer_ships = [computer_cruiser, computer_submarine]
+    @computer_cruiser = Ship.new("Cruiser", 3)
+    @computer_submarine = Ship.new("Submarine", 2)
+    @computer_ships = [computer_cruiser, computer_submarine]
 
     computer_ships.each do |ship|
     range = computers_board.cells.keys
@@ -22,9 +22,9 @@ class Game
 
   def user_board
     @users_board = Board.new
-    user_cruiser = Ship.new("Cruiser", 3)
-    user_submarine = Ship.new("Submarine", 2)
-    user_ships = [user_cruiser, user_submarine]
+    @user_cruiser = Ship.new("Cruiser", 3)
+    @user_submarine = Ship.new("Submarine", 2)
+    @user_ships = [user_cruiser, user_submarine]
 
     puts "I have laid out my ships on the grid."
     puts "You now need to lay out your two ships.
@@ -78,41 +78,46 @@ class Game
   end
 
   def turn
+
     puts "=============COMPUTER BOARD============="
     puts computers_board.render
 
     puts "==============PLAYER BOARD=============="
     puts users_board.render(true)
 
-    puts "Enter the coordinate of your shot!"
-    user_coord_input = gets.chomp
-
     # USER SHOT
 
-    if computers_board.validate_coordinate?(user_coord_input) == true
-      computers_board.cells[user_coord_input].fire_upon
-    else
-      until computers_board.validate_coordinate?(user_coord_input) == true
-        puts "That is an invalid coordinate, please try again!"
-        puts "Enter the coordinate for your shot"
-        user_coord_input = gets.chomp
-      end
+    until computer_submarine.sunk? == true && computer_cruiser.sunk? == true || user_submarine.sunk? == true && user_cruiser.sunk? == true
 
-      computers_board.cells[user_coord_input].fire_upon
 
-    end
+      puts "Enter the coordinate of your shot!"
+      user_coord_input = gets.chomp
+
+      if computers_board.validate_coordinate?(user_coord_input) == true && computers_board.cells[user_coord_input].fired_upon? == false
+
+          computers_board.cells[user_coord_input].fire_upon
+
+        else
+          until computers_board.validate_coordinate?(user_coord_input) == true && computers_board.cells[user_coord_input].fired_upon? == false
+            puts "That is an invalid coordinate, please try again!"
+            puts "Enter the coordinate for your shot"
+            user_coord_input = gets.chomp
+          end
+
+          computers_board.cells[user_coord_input].fire_upon
+
+        end
 
     # COMPUTER SHOT
 
 
     cell_range = users_board.cells.keys
     cell_range_array = cell_range.sample(1).join
-    #require 'pry' ;binding.pry
 
-    if users_board.cells[cell_range_array].cell_health == 0
+    if users_board.cells[cell_range_array].fired_upon? == false
        users_board.cells[cell_range_array].fire_upon
     else
-      until users_board.cells[cell_range_array].cell_health != 0
+      until users_board.cells[cell_range_array].fired_upon? == false
         cell_range_array = cell_range.sample(1).join
       end
         users_board.cells[cell_range_array].fire_upon
@@ -128,12 +133,31 @@ class Game
     puts "==============PLAYER BOARD=============="
     puts users_board.render(true)
 
+    if  computers_board.cells[user_coord_input].render == "H"
+      puts "Your shot on #{user_coord_input} was a hit."
+      elsif computers_board.cells[user_coord_input].render == "M"
+        puts "Your shot on #{user_coord_input} was a miss."
+      else computers_board.cells[user_coord_input].render == "X"
+        puts "Your shot on #{user_coord_input} sunk my ship!"
+    end
 
-    puts "Your shot on #{user_coord_input} was a #{}"
-    puts "My shot on #{cell_range_array} was a #{}"
 
+    if  users_board.cells[cell_range_array].render == "H"
+      puts "My shot on #{cell_range_array} was a hit."
+      elsif users_board.cells[cell_range_array].render == "M"
+        puts "My shot on #{cell_range_array} was a miss."
+      else users_board.cells[cell_range_array].render == "X"
+        puts "My shot on #{cell_range_array} sunk your ship!"
+    end
 
-    
+   end
+
+   if computer_submarine.sunk? == true && computer_cruiser.sunk? == true
+     puts "You won!"
+
+    else user_submarine.sunk? == true && user_cruiser.sunk? == true
+      puts "I won!"
+   end
 
   end
 
