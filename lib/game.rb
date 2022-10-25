e334d3c5a474140471da3ca37b310a2340ab1e3c
KaraJH <karas.jones@yahoo.com>
2022-10-25 12:23:37 -0600

    feat: Adds the user and computer shots in the turn method

--- lib/game.rb
+++ lib/game.rb
@@ -21,7 +21,7 @@ class Game
   end
 
   def user_board
-    users_board = Board.new
+    @users_board = Board.new
     user_cruiser = Ship.new("Cruiser", 3)
     user_submarine = Ship.new("Submarine", 2)
     user_ships = [user_cruiser, user_submarine]
@@ -78,7 +78,63 @@ class Game
   end
 
   def turn
-     puts computers_board.render
-  end 
+    puts "=============COMPUTER BOARD============="
+    puts computers_board.render
+
+    puts "==============PLAYER BOARD=============="
+    puts users_board.render(true)
+
+    puts "Enter the coordinate of your shot!"
+    user_coord_input = gets.chomp
+
+    # USER SHOT
+
+    if computers_board.validate_coordinate?(user_coord_input) == true
+      computers_board.cells[user_coord_input].fire_upon
+    else
+      until computers_board.validate_coordinate?(user_coord_input) == true
+        puts "That is an invalid coordinate, please try again!"
+        puts "Enter the coordinate for your shot"
+        user_coord_input = gets.chomp
+      end
+
+      computers_board.cells[user_coord_input].fire_upon
+
+    end
+
+    # COMPUTER SHOT
+
+
+    cell_range = users_board.cells.keys
+    cell_range_array = cell_range.sample(1).join
+    #require 'pry' ;binding.pry
+
+    if users_board.cells[cell_range_array].cell_health == 0
+       users_board.cells[cell_range_array].fire_upon
+    else
+      until users_board.cells[cell_range_array].cell_health != 0
+        cell_range_array = cell_range.sample(1).join
+      end
+        users_board.cells[cell_range_array].fire_upon
+    end
+
+
+
+    #RESULTS
+
+    puts "=============COMPUTER BOARD============="
+    puts computers_board.render
+
+    puts "==============PLAYER BOARD=============="
+    puts users_board.render(true)
+
+
+    puts "Your shot on #{user_coord_input} was a #{}"
+    puts "My shot on #{cell_range_array} was a #{}"
+
+
+    
+
+  end
 
 end
