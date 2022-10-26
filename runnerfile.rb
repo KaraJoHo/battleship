8302d578018b22d3c4e1e906b616099a931b8b8f
Brian Hayes <Brian.hayes1200@gmail.com>
2022-10-26 14:35:39 -0600

    loop game

--- runnerfile.rb
+++ runnerfile.rb
@@ -2,10 +2,13 @@ require './lib/cell'
 require './lib/ship'
 require './lib/board'
 require './lib/game'
-
+puts "enter username: \nOr enter exit to quit the game"
+exit_input = gets.chomp
+until exit_input == 'exit'
 game = Game.new
-game .start_menu
+game.start_menu
 game.computer_board
 game.user_board 
 game.turn
 game.ending_statement
+end 
